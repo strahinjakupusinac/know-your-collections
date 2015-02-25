625d1039749d65fd1be1592cfdb292d4b7daed79
Strahinja Kupusinac <strahinja.kupusinac@codecentric.de>
2015-02-25 10:24:46 +0100

    Updated maps experiments

--- src/collections/MapsFun.java
+++ src/collections/MapsFun.java
@@ -1,5 +1,6 @@
 package collections;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -9,26 +10,43 @@ public class MapsFun {
 
 	public static void main(String[] args) {
 		
-		HashMap<Integer, Integer> mapA = new HashMap<Integer, Integer>(1 << 14);
-		Map<Integer, Integer> mapB = new LinkedHashMap<Integer, Integer>(1 << 14);
+		Map<Integer, Integer> mapA = new HashMap<Integer, Integer>();
+		Map<Integer, Integer> mapB = new LinkedHashMap<Integer, Integer>();
+		Map<Integer, Integer> mapC = new HashMap<Integer, Integer>(1 << 19);
+		Map<Integer, Integer> mapD = new LinkedHashMap<Integer, Integer>(1 << 19);
 
 		
-		System.out.println("Experiment 1: put");
+		System.out.println("Experiment 1: put - default capacity");
 		
 		long startTime = System.nanoTime();
-		for (int i = 0; i < 10_000; i++) {
+		for (int i = 0; i < 100_000; i++) {
 			mapA.put(i, i);
 		}
 		System.out.println(System.nanoTime() - startTime);
 		
 		startTime = System.nanoTime();
-		for (int i = 0; i < 10_000; i++) {
+		for (int i = 0; i < 100_000; i++) {
 			mapB.put(i, i);
 		}
 		System.out.println(System.nanoTime() - startTime);
 		
 		
-		System.out.println("\nExperiment 2: iterate");
+		System.out.println("\nExperiment 2: put - predefined capacity");
+		
+		startTime = System.nanoTime();
+		for (int i = 0; i < 100_000; i++) {
+			mapC.put(i, i);
+		}
+		System.out.println(System.nanoTime() - startTime);		
+		
+		startTime = System.nanoTime();
+		for (int i = 0; i < 100_000; i++) {
+			mapD.put(i, i);
+		}
+		System.out.println(System.nanoTime() - startTime);		
+		
+		
+		System.out.println("\nExperiment 3: iterate");
 		
 		startTime = System.nanoTime();
 		for (Entry<Integer, Integer> entry : mapA.entrySet()) {
@@ -43,18 +61,31 @@ public class MapsFun {
 		System.out.println(System.nanoTime() - startTime);
 		
 		
-		System.out.println("\nExperiment 3: remove");
+		System.out.println("\nExperiment 4: remove");
 		
 		startTime = System.nanoTime();
-		for (int i = 0; i < 10_000; i++) {
+		for (int i = 0; i < 100_000; i++) {
 			mapA.remove(i);
 		}
 		System.out.println(System.nanoTime() - startTime);
 		
 		startTime = System.nanoTime();
-		for (int i = 0; i < 10_000; i++) {
+		for (int i = 0; i < 100_000; i++) {
 			mapB.remove(i);
 		}
 		System.out.println(System.nanoTime() - startTime);
+		
+		try {
+			Field tableField = HashMap.class.getDeclaredField("table");
+			tableField.setAccessible(true);
+			Object[] table = (Object[]) tableField.get(mapA);
+			System.out.println("mapA capacity after removal: " + (table == null ? 0 : table.length));
+			table = (Object[]) tableField.get(mapB);
+			System.out.println("mapB capacity after removal: " + (table == null ? 0 : table.length));
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+				
 	}
 }
